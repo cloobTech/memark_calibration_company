605a6c6cfb1de2f7222457eaaf86291f9cbc096c
cloobtech <belkid98@gmail.com>
2025-05-02 13:44:13 +0100

    Gallery Updated

--- src/components/ServiceStationGallery.jsx
+++ src/components/ServiceStationGallery.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const ServiceStationGallery = ({ open, service_station, images }) => {
   return (
-    <div className="bg-gray-200 p-4 rounded-lg shadow-md max-h-[520px] md:max-h-[700px] overflow-y-auto">
-      <h3 className="text-gray-800 text-center mb-4">{`${service_station}`}</h3>
+    <div className="bg-gray-200 px-4 rounded-lg shadow-md max-h-[520px] md:max-h-[700px] overflow-y-auto relative">
+      <h3 className="text-gray-800 mt-4 text-center mb-4 sticky top-0 bg-gray-200 z-10 p-2">
+        {`${service_station}`}
+      </h3>
       <div className="grid  grid-cols-3 gap-4">
         {images.map((image, index) => (
           <img
